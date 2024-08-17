c40c676b98d1a9448fba1810eeea8f7e19d5770a
Byoungseon Jeon <bjeon11@gmail.com>
2024-08-16 20:48:55 -0400

    Add files via upload

---
+++ rocm/cmake_cuda/src/vectorAdd.cu
@@ -0,0 +1,23 @@
+#include <cuda_runtime.h>
+
+#include <helper_cuda.h>
+/**
+ * CUDA Kernel Device code
+ *
+ * Computes the vector addition of A and B into C. The 3 vectors have the same
+ * number of elements numElements.
+ */
+__global__ void vectorAdd(const float *A, const float *B, float *C,
+                          int numElements) {
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+  if (i < numElements) {
+    C[i] = A[i] + B[i] + 0.0f;
+  }
+}
+
+void callGPU(int blocksPerGrid, int threadsPerBlock, float *d_A, float *d_B, float *d_C, int numElements )
+{
+  vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
+}
+
